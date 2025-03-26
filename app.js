59ac07ddff1c8cf1219a7a7bd59d1cf5b780c734
quangb1910690 <minhtu30112002@gmail.com>
2025-03-26 22:11:50 +0700

    final commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = 3000;
+const cors = require("cors");
+const route = require("./routes");
+
+const db = require("./app/utils/mongodb.util");
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "upload")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+db.connect();
+
+route(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
